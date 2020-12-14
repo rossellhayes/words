1645c3cfd112215c34d075d397d6077c78e08ce3
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-12-14 14:41:43 -0800

    Add "tumor" to list of potentially offensive words

--- data-raw/offensive.R
+++ data-raw/offensive.R
@@ -1,5 +1,7 @@
+library(magrittr)
+
 offensive <- readLines("data-raw/bad-words.txt")[-1] %>%
-  c("pagan") %>%
+  c("pagan", "tumor") %>%
   sort()
 
 usethis::use_data(offensive, overwrite = TRUE)
